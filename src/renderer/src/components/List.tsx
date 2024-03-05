c5746ebffa99e955624ab883bf89cec4d919148f
Alexandre Dias <alexdias0831@gmail.com>
2024-03-04 21:34:36 -0300

    add nextui

--- src/renderer/src/components/List.tsx
+++ src/renderer/src/components/List.tsx
@@ -1,3 +1,4 @@
+import { Button, Card } from '@nextui-org/react'
 import { Task } from '@renderer/Task'
 import { ReactElement } from 'react'
 
@@ -13,12 +14,12 @@ function List({ tarefas }: ListProps): ReactElement {
       <ul>
         {tarefas.length > 0 &&
           tarefas.map((tarefa) => (
-            <li key={tarefa.title}>
+            <Card key={tarefa.title}>
               <strong>{tarefa.title}</strong>
-              <button>excluir</button>
-              <button>editar</button>
-              <button>completar</button>
-            </li>
+              <Button isIconOnly color="danger">excluir</Button>
+              <Button isIconOnly color="primary" >editar</Button>
+              <Button isIconOnly color="success" >completar</Button>
+            </Card>
           ))}
       </ul>
     </>
